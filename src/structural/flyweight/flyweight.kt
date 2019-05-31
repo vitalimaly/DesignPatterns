52e685d002a817b60cacb129897fec452df22fa2
Vitalii Malyi <vitaliimalone@gmail.com>
2019-06-01 00:09:43 +0300

    Add proxy & flyweight

--- src/structural/flyweight/flyweight.kt
+++ src/structural/flyweight/flyweight.kt
@@ -1,2 +1,48 @@
 package structural.flyweight
 
+fun main() {
+    val forest = Forest()
+    forest.plantTree(1, 1, "Oak", "brown", 0x11)
+    forest.plantTree(1, 2, "Oak", "brown", 0x11)
+    forest.plantTree(2, 1, "Oak", "brown", 0x11)
+    forest.draw("canvas")
+}
+
+class Tree(val x: Int, val y: Int, val type: TreeType) {
+    fun draw(canvas: String) {
+        type.draw(canvas, x, y)
+    }
+}
+
+class Forest {
+    val trees = mutableListOf<Tree>()
+
+    fun plantTree(x: Int, y: Int, name: String, color: String, textureResId: Int) {
+        val type = TreeFactory.getTreeType(name, color, textureResId)
+        val tree = Tree(x, y, type)
+        trees.add(tree)
+    }
+
+    fun draw(canvas: String) {
+        trees.forEach { it.draw(canvas) }
+    }
+}
+
+class TreeType(val name: String, val color: String, val textureResId: Int) {
+    fun draw(canvas: String, x: Int, y: Int) {
+        println("draw")
+    }
+}
+
+object TreeFactory {
+    val treeTypes = mutableListOf<TreeType>()
+
+    fun getTreeType(name: String, color: String, textureResId: Int): TreeType {
+        var type = treeTypes.find { it.name == name && it.color == color && it.textureResId == textureResId }
+        if (type == null) {
+            type = TreeType(name, color, textureResId)
+            treeTypes.add(type)
+        }
+        return type
+    }
+}
--- src/structural/proxy/proxy.kt
+++ src/structural/proxy/proxy.kt
@@ -1,2 +1,75 @@
 package structural.proxy
 
+import kotlin.random.Random
+
+fun main() {
+    val youtubeService = ThirdPartyYoutubeClass()
+    val youtubeProxy = CachedYoutubeClass(youtubeService)
+    val youtubeManager = YoutubeManager(youtubeProxy)
+    youtubeManager.reactOnUserInput()
+}
+
+class YoutubeManager(val service: ThirdPartyYoutubeLib) {
+    fun renderVideoPage(id: String) {
+        println(service.getVideoInfo(id))
+    }
+
+    fun renderListPanel() {
+        println(service.listVideos())
+    }
+
+    fun reactOnUserInput(id: String = "default") {
+        renderVideoPage(id)
+        renderListPanel()
+    }
+}
+
+interface ThirdPartyYoutubeLib {
+    fun listVideos(): List<Any>
+    fun getVideoInfo(id: String): String
+    fun downloadVideo(id: String)
+}
+
+class ThirdPartyYoutubeClass : ThirdPartyYoutubeLib {
+    override fun listVideos(): List<Any> {
+        return listOf(Any(), Any())
+    }
+
+    override fun getVideoInfo(id: String): String {
+        return Random.nextBytes(10).contentToString()
+    }
+
+    override fun downloadVideo(id: String) {
+        println("downloading")
+    }
+}
+
+class CachedYoutubeClass(private val service: ThirdPartyYoutubeClass) : ThirdPartyYoutubeLib {
+    private var listCache = listOf<Any>()
+    private var videoInfoCache: String = ""
+    var needReset: Boolean = false
+
+    override fun listVideos(): List<Any> {
+        if (listCache.isEmpty() || needReset) {
+            listCache = service.listVideos()
+        }
+        return listCache
+    }
+
+    override fun getVideoInfo(id: String): String {
+        if (videoInfoCache.isEmpty()) {
+            videoInfoCache = service.getVideoInfo(id)
+        }
+        return videoInfoCache
+    }
+
+    override fun downloadVideo(id: String) {
+        if (!downloadExists(id) || needReset) {
+            service.downloadVideo(id)
+        }
+    }
+
+    private fun downloadExists(id: String): Boolean {
+        return Random.nextBoolean()
+    }
+}
